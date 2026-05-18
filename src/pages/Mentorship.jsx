7122a2cf6bba723f58728e6a63063d41d26bfe74
mvetrici <161236886+mvetrici@users.noreply.github.com>
2026-05-18 17:17:42 -0400

    Update Mentorship.jsx

--- src/pages/Mentorship.jsx
+++ src/pages/Mentorship.jsx
@@ -73,7 +73,7 @@ function Mentorship() {
                   src="./assets/mentee-book.pdf"
                   style={{
                     width: '100%',
-                    height: '1100px',
+                    height: '550px',
                   }}
                 >
                   Loading…
@@ -83,7 +83,7 @@ function Mentorship() {
                   src="./assets/mentor-book.pdf"
                   style={{
                     width: '100%',
-                    height: '1100px',
+                    height: '550px',
                   }}
                 >
                   Loading…
